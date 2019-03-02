238d2c4b57f12d4edf9b8e4c4d30aeed1041e250
Nathan Gonzalez <ncgonz@gmail.com>
2019-03-02 11:30:26 -0600

    adding a couple of implementation examples

--- LetterLoops/Program.cs
+++ LetterLoops/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LetterLoops
 {
@@ -6,7 +8,26 @@ namespace LetterLoops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter some letters!");
+            var input = Console.ReadLine().ToLower();
+            var loopCount = 0;
+            //var myCharacters = new List<string>();
+
+            //foreach (var character in input)
+            //{
+            //    var letterString = character.ToString().ToUpper();
+            //    letterString += new string(character, loopCount);
+
+            //    myCharacters.Add(letterString);
+
+            //    loopCount++;
+            //}
+
+            //Console.WriteLine(string.Join('-', myCharacters));
+
+            var output = string.Join('-', input.ToCharArray().Select(c => c.ToString().ToUpper() + new string(c, loopCount++)));
+            Console.WriteLine(output);
+            Console.ReadLine();
         }
     }
 }
